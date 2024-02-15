7e70664b82b401b9a46f374b56d71599fd866ff9
victor1206 <victor_rivas89@hotmail.com>
2024-02-15 17:16:45 -0600

    Cambios nuevos

---
+++ SysSeguridadG05.DAL/RolDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//*****************************
+using Microsoft.EntityFrameworkCore;
+using SysSeguridadG05.EN;
+
+namespace SysSeguridadG05.DAL
+{
+    public class RolDAL
+    {
+        public static async Task<int> CrearAsync(Rol pRol)
+        { 
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            { 
+                dbContexto.Add(pRol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+                rol.Nombre = pRol.Nombre;
+                dbContexto.Update(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+    }
+}
