537c9e5fb49b4754ea24d9de0d95f841a8dae426
victor1206 <victor_rivas89@hotmail.com>
2024-09-03 15:06:39 -0600

    Agregar proyecto base de web Api Restfull

---
+++ SysSeguridadG05.WebApi/Controllers/RolController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SysSeguridadG05.BL;
+using SysSeguridadG05.EN;
+using System.Text.Json;
+
+namespace SysSeguridadG05.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        private RolBL rolBl = new RolBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Rol>> Get()
+        {
+            return await rolBl.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Rol> Get(int id)
+        {
+            Rol rol = new Rol();
+            rol.Id = id;
+            return await rolBl.ObtenerPorIdAsync(rol);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] object pRol)
+        {
+            try
+            {
+                var option = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                string strRol = JsonSerializer.Serialize(pRol);
+                Rol rol = JsonSerializer.Deserialize<Rol>(strRol, option);
+                await rolBl.CrearAsync(rol);
+                return Ok();
+            }
+            catch (Exception Ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
